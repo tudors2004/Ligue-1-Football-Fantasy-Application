a519e35ff9cabb98696e1fd18190888c0bc3658e
Tudor Sarghiuta <149654961+tudors2004@users.noreply.github.com>
2024-11-19 23:47:21 +0200

    First Commit

---
+++ src/main/java/com/l1/fantasyfr/Ligue1FantasyApplication.java
@@ -0,0 +1,13 @@
+package com.l1.fantasyfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ligue1FantasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ligue1FantasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l1/fantasyfr/Ligue1FantasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l1.fantasyfr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ligue1FantasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
